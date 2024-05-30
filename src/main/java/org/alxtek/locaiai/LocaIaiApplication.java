3d6e1c4c513999640d1844d87fe680c9a847c13e
alex <alextchoupou@gmail.com>
2024-05-30 03:36:43 +0200

    Initial commit

---
+++ src/main/java/org/alxtek/locaiai/LocaIaiApplication.java
@@ -0,0 +1,13 @@
+package org.alxtek.locaiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaIaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocaIaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alxtek/locaiai/LocaIaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alxtek.locaiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaIaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
